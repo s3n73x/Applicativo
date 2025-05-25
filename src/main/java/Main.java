6d9993e9083a8eb1a70a4bf0e954b06c038dc347
s3n73x <117867262+s3n73x@users.noreply.github.com>
2025-05-25 21:03:03 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+package Model;
+
+import GUI.*;
+import GUI.FinestraRegister;
+import java.util.List;
+
+public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new FinestraLogin();
     }
 }
